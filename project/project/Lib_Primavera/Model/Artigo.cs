ba773a0fce43e6663ead18d62cfbb9b0529c7978
Henrique Ferrolho <henriqueferrolho@gmail.com>
2015-10-23 10:58:53 +0100

    added project base

---
+++ project/project/Lib_Primavera/Model/Artigo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project.Lib_Primavera.Model
+{
+    public class Artigo
+    {
+        public string CodArtigo
+        {
+            get;
+            set;
+        }
+
+        public string DescArtigo
+        {
+            get;
+            set;
+        }
+
+    }
+}
