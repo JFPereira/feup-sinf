361eff7b9bf945bb5de4368357b84f71ab179fc2
Anaís Dias <dias4993@gmail.com>
2015-10-29 23:28:59 +0000

    Incluido GetTopClientesArtigo

--- project/project/Lib_Primavera/PriIntegration.cs
+++ project/project/Lib_Primavera/PriIntegration.cs
@@ -392,7 +392,7 @@ namespace project.Lib_Primavera
             else
                 return null;
         }
-        /*
+        
         public static Lib_Primavera.Model.Artigo GetTopClientesArtigo(Model.Artigo artigo)
         {
             StdBELista objList;
@@ -400,10 +400,10 @@ namespace project.Lib_Primavera
             List<Model.CabecDoc> sales = new List<Model.CabecDoc>();
 
             bool companyInitialized = initCompany();
-
+            
             if (companyInitialized)
             {
-                objList = PriEngine.Engine.Consulta("SELECT CabecDoc.Data, CabecDoc.Entidade, CabecDoc.Nome, CabecDoc.NumDoc, CabecDoc.NumContribuinte, CabecDoc.TotalMerc, CabecDoc.TotalIva FROM CabecDoc");
+                objList = PriEngine.Engine.Consulta("SELECT CabecDoc.Data, CabecDoc.Entidade, CabecDoc.Nome, CabecDoc.NumDoc, CabecDoc.NumContribuinte, CabecDoc.TotalMerc, CabecDoc.TotalIva FROM LinhasDoc, CabecDoc WHERE LinhasDoc.IdCabecDoc = CabecDoc.Id AND LinhasDoc.Artigo = " + artigo.CodArtigo);
                 while (!objList.NoFim())
                 {
                     sales.Add(new Model.CabecDoc
@@ -423,11 +423,47 @@ namespace project.Lib_Primavera
                     objList.Seguinte();
                 }
 
-                return sales;
+                List<Items.TopClientsItem> result = new List<Items.TopClientsItem>();
+
+                double totalSalesVolume = 0;
+
+                foreach (Model.CabecDoc sale in sales)
+                {
+                    if (result.Exists(e => e.nif == sale.NumContribuinte))
+                    {
+                        result.Find(e => e.nif == sale.NumContribuinte).salesVolume += (sale.TotalMerc + sale.TotalIva);
+                        result.Find(e => e.nif == sale.NumContribuinte).numPurchases++;
+                    }
+                    else
+                    {
+                        result.Add(new Items.TopClientsItem
+                        {
+                            entity = sale.Entidade,
+                            name = sale.Nome,
+                            nif = sale.NumContribuinte,
+                            salesVolume = sale.TotalMerc + sale.TotalIva,
+                            percentage = "",
+                            numPurchases = 1
+                        });
+                    }
+
+                    totalSalesVolume += (sale.TotalMerc + sale.TotalIva);
+                }
+
+                result = result.OrderBy(e => e.salesVolume).Reverse().Take(10).ToList();
+
+                foreach (Items.TopClientsItem client in result)
+                    client.percentage += Math.Round(client.salesVolume / totalSalesVolume * 100, 2) + " %";
+
+                artigo.TopClientes = result;
+
+
+                return artigo;
             }
             else
                 return null;
-        }*/
+       
+        }
         /*
             if (PriEngine.InitializeCompany(project.Properties.Settings.Default.Company.Trim(), project.Properties.Settings.Default.User.Trim(), project.Properties.Settings.Default.Password.Trim()) == true)
             {
