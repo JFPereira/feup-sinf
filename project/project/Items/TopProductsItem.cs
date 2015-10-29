bd9e4447de5b54b45e55a332d3c82ffffd9eb31d
Anaís Dias <dias4993@gmail.com>
2015-10-29 16:41:08 +0000

    added TopProductsItem

---
+++ project/project/Items/TopProductsItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project.Items
+{
+    public class TopProductsItem
+    {
+
+        public string codArtigo
+        {
+            get;
+            set;
+        }
+        public string name
+        {
+            get;
+            set;
+        }
+        public double quantity
+        {
+            get;
+            set;
+        }
+        public string percentage
+        {
+            get;
+            set;
+        }
+        public double salesVolume
+        {
+            get;
+            set;
+        }
+
+    }
+}
