1dbed58bef8e013b53fb166fdb67a4b47c4396a2
Henrique Ferrolho <henriqueferrolho@gmail.com>
2015-10-30 10:27:03 +0000

    Added missing item

---
+++ project/project/Items/RegSalesBookingItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project.Items
+{
+    public class RegSalesBookingItem
+    {
+        public string pais
+        {
+            get;
+            set;
+        }
+
+        public double valorVendas
+        {
+            get;
+            set;
+        }
+
+    }
+}
