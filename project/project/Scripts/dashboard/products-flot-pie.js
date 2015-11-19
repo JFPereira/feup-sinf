224b8e41f7d8327b6982c4e8e4b91b5657b29585
Anaís Dias <dias4993@gmail.com>
2015-11-19 15:24:22 +0000

    missing scripts added

---
+++ project/project/Scripts/dashboard/products-flot-pie.js
@@ -0,0 +1,53 @@
+﻿$(function () {
+
+    $.ajax({
+        dataType: "json",
+        url: "http://localhost:49328/api/products/top",
+        success: function (products) {
+            products = JSON.parse(products);
+
+            var data = [];
+            $.each(products, function (i) {
+                data.push({ label: products[i].description, data: products[i].percentage });
+            });
+
+            console.log(data);
+
+            $.plot("#placeholderB", data, {
+                series: {
+                    pie: {
+                        show: true,
+                        radius: 1,
+                        label: {
+                            show: true,
+                            radius: 1,
+                            formatter: labelFormatter,
+                            background: {
+                                opacity: 0.8
+                            }
+                        }
+                    }
+                },
+                grid: {
+                    hoverable: true,
+                    clickable: false
+                },
+                legend: {
+                    show: true
+                }
+            });
+        }
+    })
+});
+
+// A custom label formatter used by several of the plots
+
+function labelFormatter(label, series) {
+    return "<div style='font-size:8pt; text-align:center; padding:2px; color:white;'>" + label + "<br />" + Math.round(series.percent) + "%</div>";
+}
+
+//
+
+function setCode(lines) {
+    $("#code").text(lines.join("\n"));
+}
---
+++ project/project/Scripts/dashboard/top-sales-datatable.js
@@ -0,0 +1,26 @@
+﻿$(function () {
+
+    $.ajax({
+        dataType: "json",
+        url: "http://localhost:49328/api/sales/top",
+        success: function (sales) {
+            sales = JSON.parse(sales);
+
+            var dataS = [];
+            $.each(sales, function (i) {
+                dataS.push([sales[i].entity, sales[i].purchaseValue, sales[i].numPurchases ]);
+            });
+
+            console.log(dataS);
+
+            $('#topsales').DataTable({
+                data: dataS,
+                columns: [
+                    { title: "Entity" },
+                    { title: "Purchase Value" },
+                    { title: "Nr Purchases" }
+                ]
+            });
+        }
+    })
+});
