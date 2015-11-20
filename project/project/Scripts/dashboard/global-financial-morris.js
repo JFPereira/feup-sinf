41f9efc21b82b59542c74c81cdfbdf527b3ff467
Anaís Dias <dias4993@gmail.com>
2015-11-20 10:41:37 +0000

    added global financial script

---
+++ project/project/Scripts/dashboard/global-financial-morris.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+
+    $.ajax({
+        dataType: "json",
+        url: "http://localhost:49328/api/financial/global",
+        success: function (global) {
+            global = JSON.parse(global);
+
+            var dataG = [];
+            $.each(global, function (i) {
+                var mes = global[i].Ano + "-" + global[i].Mes;
+                dataG.push({ month: mes, sales: global[i].Vendas, purchases: global[i].Compras });
+            });
+
+            var months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+
+            console.log(dataG);
+
+            new Morris.Line({
+                // ID of the element in which to draw the chart.
+                element: 'globalfinancial',
+                // Chart data records -- each entry in this array corresponds to a point on
+                // the chart.
+                data: dataG,
+                // The name of the data record attribute that contains x-values.
+                xkey: 'month', 
+                // A list of names of data record attributes that contain y-values.
+                ykeys: ['sales','purchases'],
+                // Labels for the ykeys -- will be displayed when you hover over the
+                // chart.
+                labels: ['Sales', 'Purchases'],
+
+            });
+        }
+    })
+});
+
