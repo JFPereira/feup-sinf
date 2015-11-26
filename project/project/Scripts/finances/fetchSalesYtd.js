af99965dd86b62573980e6b74f2b39a9082fcccd
Henrique Ferrolho <henriqueferrolho@gmail.com>
2015-11-26 17:02:10 +0000

    [Finances] Purchases YTD and Sales YTD KPIs done.

---
+++ project/project/Scripts/finances/fetchSalesYtd.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $.ajax({
+        dataType: "json",
+        url: "http://localhost:49328/api/financial/sales",
+        success: function (content) {
+            content = JSON.parse(content);
+
+            var domElem = document.getElementById('salesYtdPlaceholder');
+            domElem.innerHTML = content;
+        }
+    })
+});
