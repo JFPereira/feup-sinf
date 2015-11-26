019d76adf38499bf73436f02414a66634a237028
Joao-Ramos <joaoramos-13@hotmail.com>
2015-11-26 23:56:06 +0000

    Finished sales page overall... Maybe add a few links

---
+++ project/project/Items/SalesGrowthItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project.Items
+{
+    public class SalesGrowthItem
+    {
+        public string Nome
+        {
+            get;
+            set;
+        }
+        public double Percentagem
+        {
+            get;
+            set;
+        }
+        public double Valor
+        {
+            get;
+            set;
+        }
+
+    }
+}
+
