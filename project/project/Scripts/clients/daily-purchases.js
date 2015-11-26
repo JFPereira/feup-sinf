9afb51a24dce3d97ad539018402326a8bc58beb8
JFPereira <pereiraffjoao1993@gmail.com>
2015-11-26 17:47:54 +0000

    starting purchases view on graph; sales volume done

--- project/project/Scripts/clients/daily-purchases.js
+++ project/project/Scripts/clients/daily-purchases.js
@@ -4,9 +4,9 @@
     }, 10);
 });
 
-function doPlot(purchases, days) {
-    $.plot("#clientDailyPurchasesPlaceholder", [
-        { label: "Purchases", data: purchases }
+function doPlot(salesVolume, purchases, days) {
+    var plot = $.plot("#clientDailyPurchasesPlaceholder", [
+        { label: "Sales Volume", data: salesVolume }
     ],
     {
         series: {
@@ -20,7 +20,7 @@ function doPlot(purchases, days) {
         yaxis: {
             ticks: 1,
             min: 0,
-            tickFormatter: function (val, axis) { return val < axis.max ? val : val + "  Volume"; }
+            tickFormatter: function (val, axis) { return val < axis.max ? val + "€" : val + "€  Volume"; }
         },
         grid: {
             backgroundColor: { colors: ["#fff", "#eee"] },
@@ -29,9 +29,34 @@ function doPlot(purchases, days) {
                 right: 1,
                 bottom: 2,
                 left: 2
-            }
+            },
+            hoverable: true,
+            clickable: true
         }
     });
+
+    $("<div id='tooltip'></div>").css({
+        position: "absolute",
+        display: "none",
+        border: "1px solid #fdd",
+        padding: "2px",
+        "background-color": "#fee",
+        opacity: 0.80
+    }).appendTo("body");
+
+    $("#clientDailyPurchasesPlaceholder").bind("plothover", function (event, pos, item) {
+        if (item) {
+            var day = item.datapoint[0],
+                value = item.datapoint[1];
+
+            $("#tooltip").html(item.series.label + " of day " + day + ": " + value + "€")
+                .css({ top: item.pageY + 5, left: item.pageX + 5 })
+                .fadeIn(200);
+        } else {
+            $("#tooltip").hide();
+        }
+    });
+
 };
 
 function updateDailyPurchases() {
@@ -58,7 +83,7 @@ function updateDailyPurchases() {
                 salesVolume.push([item[i].day, item[i].salesVolume]);
             });
 
-            doPlot(purchases, days);
+            doPlot(salesVolume, purchases, days);
 
             $("button").click(function () {
                 doPlot($(this).text());
