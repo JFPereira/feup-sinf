e123e7774134769c819f54bf5f3e719b4c754512
JFPereira <pereiraffjoao1993@gmail.com>
2015-11-27 01:15:53 +0000

    js of products page now in folder

---
+++ project/project/Scripts/products/product-units-sold.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    var product = document.getElementById("productID").getAttribute("value");
+
+
+    $.ajax({
+        dataType: "json",
+        url: "http://localhost:49328/api/products/" + product + "/sales",
+        success: function (sales) {
+            sales = JSON.parse(sales);
+            sales = sales.Vendidos;
+
+            var s = document.getElementById('productunitssold');
+            s.innerHTML = sales;
+
+            $("#productunitssoldLoadingAnimation").remove();
+        }
+    })
+});
